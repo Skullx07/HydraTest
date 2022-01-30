d1b63756e018c412169ca96015ea704f76dde604
SKuLLx07 <93735998+Skullx07@users.noreply.github.com>
2022-01-30 14:37:48 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,18 +1,20 @@
 from telegram import InlineKeyboardMarkup
 from telegram.message import Message
 from telegram.update import Update
+from telegram import Bot
 import time
 import psutil
 from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, bot, \
-    status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock
+    status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock, UPLOAD_LOG, UPLOAD_UNAME
 from bot.helper.ext_utils.bot_utils import get_readable_message, get_readable_file_size, MirrorStatus
 from telegram.error import TimedOut, BadRequest
+from bot.helper.ext_utils.bot_utils import BotCommands
 
 def sendMessage(text: str, bot, update: Update):
     try:
         return bot.send_message(update.message.chat_id,
                             reply_to_message_id=update.message.message_id,
-                            text=text, parse_mode='HTMl')
+                            text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
     except Exception as e:
         LOGGER.error(str(e))
 
@@ -21,7 +23,112 @@ def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarku
     try:
         return bot.send_message(update.message.chat_id,
                              reply_to_message_id=update.message.message_id,
-                             text=text, reply_markup=reply_markup, parse_mode='HTMl')
+                             text=text, reply_markup=reply_markup, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+        
+        
+def sendLog(text: str, bot, update: Update):
+    try:
+        return bot.send_message(f"{UPLOAD_LOG}",
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+        
+        
+        def sendPrivate(text: str, bot, update: Update):
+    bot_d = bot.get_me()
+    b_uname = bot_d.username
+
+    try:
+        return bot.send_message(update.message.from_user.id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+        if "Forbidden" in str(e):
+            uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+            botstart = f'<a href="http://t.me/{b_uname}?start=start">Start Me</a>'
+            sendMessage(f"<b>Hey</b> {uname},\n\n<b>You didn't Started Me in Private Chat!</b>\n\n<i>Please {botstart} in PM(Private Chat) and Don't Miss Future Uploads. From Now On, I'll Give Links in Private Chat Only.</i>\n<i>For Now, Get Your Uploads From @{UPLOAD_UNAME}</i>", bot, update)
+            return
+
+
+def sendPvt(text: str, bot, update: Update):
+    bot_d = bot.get_me()
+    b_uname = bot_d.username
+
+    try:
+        return bot.send_message(update.message.from_user.id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+        if "Forbidden" in str(e):
+            uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+            sendMessage(f"<b>Hey</b> {uname}, <b>You didn't Started Me in Private Chat!</b>", bot, update)
+            return
+
+
+def sendLog(text: str, bot, update: Update):
+    try:
+        return bot.send_message(f"{UPLOAD_LOG}",
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+def sendPrivate(text: str, bot, update: Update):
+    bot_d = bot.get_me()
+    b_uname = bot_d.username
+
+    try:
+        return bot.send_message(update.message.from_user.id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+def sendLog(text: str, bot, update: Update):
+    try:
+        return bot.send_message(f"{UPLOAD_LOG}",
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+def sendPrivate(text: str, bot, update: Update):
+    bot_d = bot.get_me()
+    b_uname = bot_d.username
+
+    try:
+        return bot.send_message(update.message.from_user.id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+def sendLog(text: str, bot, update: Update):
+    try:
+        return bot.send_message(f"{UPLOAD_LOG}",
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+def sendPrivate(text: str, bot, update: Update):
+    bot_d = bot.get_me()
+    b_uname = bot_d.username
+
+    try:
+        return bot.send_message(update.message.from_user.id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, disable_web_page_preview=True, allow_sending_without_reply=True, parse_mode='HTMl')
     except Exception as e:
         LOGGER.error(str(e))
 
@@ -93,7 +200,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>DL:</b>{dlspeed}ps ⏬| <b>UL:</b>{ulspeed}ps ⏫\n"
+        msg += f"\n<b>DOWN: </b>{dlspeed}ps ⏬| <b>UP: </b>{ulspeed}ps ⏫\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -128,7 +235,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>DL:</b>{dlspeed}ps 🔻| <b>UL:</b>{ulspeed}ps 🔺\n"
+        progress += f"\n<b>DOWN: </b>{dlspeed}ps 🔻| <b>UP: </b>{ulspeed}ps 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
@@ -143,3 +250,13 @@ def sendStatusMessage(msg, bot):
             progress = "Starting DL"
         message = sendMessage(progress, bot, msg)
         status_reply_dict[msg.message.chat.id] = message
+
+def sendAddedMessage(update: Update, is_watch_command=False):
+    if is_watch_command:
+        update.effective_message.reply_html(
+            f"{update.effective_user.mention_html()}, <b>Your Requested YT-DL Link Has Been Added To</b> /{BotCommands.StatusCommand}"
+        )
+    else:
+        update.effective_message.reply_html(
+            f"{update.effective_user.mention_html()}, <b>Your Requested Task Has Been Added To</b> /{BotCommands.StatusCommand}"
+        )
