4202be9f4f4d333fc3b5a6112f52f6c5d9a77c90
SKuLLx07 <93735998+Skullx07@users.noreply.github.com>
2022-01-30 14:25:56 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -23,7 +23,7 @@ def cloneNode(update,context):
             else:
                 uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
             if uname is not None:
-                cc = f'\n\n<b>Cloned by:</b> {uname}'
+                cc = f'\n<b>Cloned by:</b> {uname}'
             sendMessage(result+ cc, context.bot, update)
     else:
         sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
